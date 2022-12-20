3a914235be0698e5834cb49b54703724b0e2889c
AliElMesaouab <alielmessouab@gmail.com>
2022-12-20 14:47:58 +0100

    init

--- js/index.js
+++ js/index.js
@@ -31,9 +31,4 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 }
 
 // ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
 
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = '/'
-})
-;
